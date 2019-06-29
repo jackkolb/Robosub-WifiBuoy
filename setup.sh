60d35ef1acd4ba43b63d976332d6e466db94b907
Jack Kolb <jackhunterkolb@gmail.com>
2019-06-28 23:18:13 -0700

    replaced files, changed setup

--- setup.sh
+++ setup.sh
@@ -1,11 +1,16 @@
 echo Created by UCR RoboSub, 2019
 echo This script installs and sets up the WiFi Buoy software, press [ENTER] to begin
-read temp
 
-echo Beginning setup
-echo Updating Raspian
-sudo apt-get update
-sudo apt-get upgrade
+echo If something doesn't work, try updating raspian (sudo apt-get upgrade)
+
+echo beginning setup
+
+echo making file backups (STORE THESE ELSEWHERE IN CASE YOU RUN THIS SCRIPT TWICE!)
+sudo cp /etc/dhcpcd.conf /etc/dhcpcd.conf.BACKUP
+sudo cp /etc/dnsmasq.conf /etc/dnsmasq.conf.BACKUP
+sudo cp /etc/hostapd.conf /etc/hostapd.conf.BACKUP
+sudo cp /etc/default/dnsmasq /etc/default/dnsmasq.BACKUP
+sudo cp /etc/default/hostapd /etc/default/hostapd.BACKUP
 
 echo installing dnsmasq and hostapd
 sudo apt-get install dnsmasq hostapd
@@ -20,16 +25,20 @@ sudo cp dhcpcd.conf.robosub /etc/dhcpcd.conf
 echo restarting dhcpcd
 sudo service dhcpcd restart
 
-echo copying over the dnsmasq.conf file
+echo copying over the dnsmasq files
 sudo cp dnsmasq.conf.robosub /etc/dnsmasq.conf
+sudo cp default.dnsmasq.conf.robosub /etc/default/dnsmasq
 
-echo copying over the hostapd.conf file
+echo copying over the hostapd files
 sudo cp hostapd.conf.robosub /etc/hostapd/hostapd.conf
+sudo cp default.hostapd.robosub /etc/hostapd/hostapd
 
-echo copying over the default/hostapd file
-sudo cp default.hostapd.robosub /etc/default/hostapd
+echo copying over the network interface
+sudo cp network.interfaces.robosub /etc/network/interfaces
 
 echo starting up the services
+sudo systemctl enable hostapd
+sudo systemctl enable dnsmasq
 sudo systemctl start hostapd
 sudo systemctl start dnsmasq
 
@@ -43,3 +52,4 @@ sudo iptables-restore < /etc/iptables.ipv4.nat
 
 echo setup complete
 echo press [ENTER] to reboot!
+sudo reboot
